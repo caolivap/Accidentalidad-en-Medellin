9525e34aabb721f4ecc4c403eaeb6378f2d8a5fc
unknown <eacaicedoc@unal.edu.cogit config --global user.name Edwin>
2018-02-24 15:27:57 -0500

    Bases de datos

--- Mapa de barrios/mb/server.R
+++ Mapa de barrios/mb/server.R
@@ -10,8 +10,31 @@ shinyServer(function(input, output) {
   output$mapb <- renderPlot({
     
     # generate bins based on input$bins from ui.R
-    base1 <- shapefile("Accidentalidad_2017.shp",encoding="UTF-8",use_iconv=TRUE) 
+    Base3 <- shapefile("Accidentalidad_2017.shp",encoding="UTF-8",use_iconv=TRUE) 
+    Base3@data <- na.omit(Base3@data)
     
+    cbind(rainbow(8),unique(Base3@data$CLASE))
+    
+    popup<-paste(Base3@data$CLASE,Base3@data$BARRIO,sep="<br/>")
+    
+    m<-leaflet()
+    m<-fitBounds(m, lng1=min(Base3@coords[,1]), 
+                 lat1=min(Base3@coords[,2]), 
+                 lng2=max(Base3@coords[,1]),
+                 lat2=max(Base3@coords[,2]))
+    m<-addProviderTiles(m,provider="OpenStreetMap.Mapnik")
+    m<-addCircleMarkers(m,
+                        lng = Base3@coords[,1],
+                        lat = Base3@coords[,2],
+                        popup = popup, 
+                        radius = 2, 
+                        stroke = FALSE,
+                        color=pal(Base3@data$CLASE),
+                        fillOpacity = 0.75
+    )
+    m<-addLegend(m,"topright",pal=pal,values=Base3@data$CLASE, 
+                 title="Tipo de accidente",
+                 labels = Base3@data$CLASE,opacity = 1)
     
     # draw the histogram with the specified number of bins
     
