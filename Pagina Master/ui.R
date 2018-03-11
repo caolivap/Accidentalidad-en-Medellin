1dcdcef050ece69848ad5fb43e796444a6d79ed7
unknown <eacaicedoc@unal.edu.cogit config --global user.name Edwin>
2018-03-11 15:08:27 -0500

    cambios finales

--- Pagina Master/ui.R
+++ Pagina Master/ui.R
@@ -24,43 +24,25 @@ shinyUI(navbarPage(theme = shinytheme("superhero"), title =  "ACCIDENTALIDAD EN 
                             #titlePanel("Resumen Descriptivo"),
                             
                             # Sidebar layout with input and output definitions ----
-                            sidebarLayout(
-                              
-                              # Sidebar panel for inputs ----
-                              sidebarPanel(
-                                
-                                # Input: Select a dataset ----
-                                selectInput("dataset", "Escoge el año:",
-                                            choices = c("2015", "2016", "2017")),
-                                
-                                # Input: Specify the number of observations to view ----
-                                numericInput("obs", "Número de observaciones que quieres ver:", 10),
-                                
-                                # Include clarifying text ----
-                                helpText("
-                                         Nota: aunque la vista de datos mostrara solo el numero especificado de observaciones,
-                                         el resumen seguira basandose en el conjunto de datos completo."),
-                                
-                                # Input: actionButton() to defer the rendering of output ----
-                                # until the user explicitly clicks the button (rather than
-                                # doing it immediately when inputs change). This is useful if
-                                # the computations required to render output are inordinately
-                                # time-consuming.
-                                actionButton("update", "Actualizar")
-                                
-                              ),
-                              # Main panel for displaying outputs ----
-                              mainPanel(
-                                
-                                # Output: Header + summary of distribution ----
-                                h4("Summary"),
-                                verbatimTextOutput("summary"),
-                                tags$video(HTML('<iframe width="560" height="315" src="https://www.youtube.com/embed/6F5_jbBmeJU" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>')),
-                                # Output: Header + table of distribution ----
-                                h4("Observaciones"),
-                                tableOutput("view")
-                              )
-                            )
+                            titlePanel(p(em("Bienvenidos a nuestra aplicacion"))),
+                            sidebarLayout(position="left",
+                                          sidebarPanel(h4("Universidad Nacional de Colombia",br(),
+                                                          "Sede Medellin"),br(),
+                                                       img(src="unal.png",height = 150, width = 150),br(),br(),
+                                                       em("Desarrollado por:"),br(),
+                                                       "Caicedo Chamorro Edwin Alexander",br(),
+                                                       "Lopez Velez Eliana Maria",br(),
+                                                       "Oliva Paredes Carlos Armando",br(),
+                                                       "Ramirez Echeverri Juan Pablo",br(),
+                                                       "Toro Zuluaga Santigo"),
+                                          mainPanel(h2("Enfoque de la Aplicacion"),br(),"Medellín es una de las ciudades
+                                                    donde la mayoría de personas tienen transporte particular, ya sea moto o carro, pero son muy 
+                                                    pocas las personas que acatan todas las normas y son responsables a la hora de hacer uso de este.
+                                                    Con esta aplicación se busca dar una guía para hacer más eficiente la distribución actual de 
+                                                    servicios de primeros auxilios prestados por la Secretaria de Salud a las personas afectadas 
+                                                    gravemente en accidentes en la ciudad")
+                                          
+                                          )
                    ),
                    tabPanel("Mapa",
                             
